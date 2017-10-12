5ee8803c1da402b3a485a8e80e1e2b396ac48820
Johannes Filter <hi@jfilter.de>
2017-10-12 20:16:18 +0200

    remove pagination check mark

--- src/PageDots.js
+++ src/PageDots.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-const PageCheckmark = ({ style }) => (
-  <Text style={{ ...styles.element, ...styles.elementCheck, ...style }}>✓</Text>
-);
-
 const PageDot = ({ isLight, selected }) => (
   <View
     style={{
@@ -19,15 +15,9 @@ const PageDot = ({ isLight, selected }) => (
 
 const PageDots = ({ isLight, pages, currentPage }) => (
   <View style={styles.container}>
-    <PageCheckmark style={{ color: 'rgba(255, 255, 255, 0)' }} />
     {Array.from(new Array(pages), (x, i) => i).map(page => (
       <PageDot key={page} selected={page === currentPage} isLight={isLight} />
     ))}
-    <PageCheckmark
-      style={{
-        color: isLight ? 'rgba(0, 0, 0, 0.3)' : 'rgba(255, 255, 255, 0.5)',
-      }}
-    />
   </View>
 );
 
