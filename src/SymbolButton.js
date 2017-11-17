3d42acb0b6daad6c8ef59b3d865a290440f5bca6
SkyzohKey <skyzohkey@protonmail.com>
2017-11-17 08:34:46 +0100

    Remove deprecated View.propTypes and update with react-native/ViewPropTypes

--- src/SymbolButton.js
+++ src/SymbolButton.js
@@ -1,17 +1,17 @@
-import { View, TouchableOpacity, Text } from 'react-native';
-import React from 'react';
-import PropTypes from 'prop-types';
+import { View, TouchableOpacity, Text, ViewPropTypes } from "react-native";
+import React from "react";
+import PropTypes from "prop-types";
 
 const SymbolButton = ({ size, onPress, style, textStyle, children }) => (
   <View
-    style={{ height: size, width: size, justifyContent: 'center', ...style }}
+    style={{ height: size, width: size, justifyContent: "center", ...style }}
   >
-    <TouchableOpacity 
-      style={{ flex: 0 }} 
+    <TouchableOpacity
+      style={{ flex: 0 }}
       onPress={onPress}
       hitSlop={{ top: 15, bottom: 15, left: 15, right: 15 }}
     >
-      <Text style={{ textAlign: 'center', fontSize: size / 1.7, ...textStyle }}>
+      <Text style={{ textAlign: "center", fontSize: size / 1.7, ...textStyle }}>
         {children}
       </Text>
     </TouchableOpacity>
@@ -21,13 +21,13 @@ const SymbolButton = ({ size, onPress, style, textStyle, children }) => (
 SymbolButton.propTypes = {
   size: PropTypes.number.isRequired,
   onPress: PropTypes.func,
-  style: View.propTypes.style,
-  textStyle: Text.propTypes.style,
+  style: ViewPropTypes.style,
+  textStyle: Text.propTypes.style
 };
 
 SymbolButton.defaultProps = {
   style: null,
-  textStyle: null,
+  textStyle: null
 };
 
 export default SymbolButton;
