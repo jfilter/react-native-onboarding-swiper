2398b5e74c9a8b5c8fd005e42d06b5e2546b7ddb
Johannes Filter <hi@jfilter.de>
2018-04-08 13:14:17 +0200

    add fade in animation for done button (close #11)

--- src/buttons/DoneButton.js
+++ src/buttons/DoneButton.js
@@ -1,21 +1,48 @@
+import { Animated } from 'react-native';
 import React from 'react';
 
-import SymbolButton from './SymbolButton';
 import { BUTTON_SIZE, MARGIN_RIGHT, getDefaultStyle } from './util';
+import SymbolButton from './SymbolButton';
 
-const DoneButton = ({ isLight, ...rest }) => (
-  <SymbolButton
-    size={BUTTON_SIZE}
-    textStyle={getDefaultStyle(isLight)}
-    style={{
-      borderRadius: BUTTON_SIZE / 2,
-      backgroundColor: 'rgba(255, 255, 255, 0.10)',
-      margin: MARGIN_RIGHT,
-    }}
-    {...rest}
-  >
-    ✓
-  </SymbolButton>
-);
+class DoneButton extends React.Component {
+  state = {
+    fadeAnim: new Animated.Value(0),
+  };
+
+  componentDidMount() {
+    setTimeout(() => {
+      Animated.timing(this.state.fadeAnim, {
+        toValue: 1,
+        duration: 1000,
+      }).start();
+    }, 1000);
+  }
+
+  render() {
+    const { isLight, ...rest } = this.props;
+    const { fadeAnim } = this.state;
+
+    return (
+      <Animated.View
+        style={{
+          opacity: fadeAnim,
+        }}
+      >
+        <SymbolButton
+          size={BUTTON_SIZE}
+          textStyle={getDefaultStyle(isLight)}
+          style={{
+            borderRadius: BUTTON_SIZE / 2,
+            backgroundColor: 'rgba(255, 255, 255, 0.10)',
+            margin: MARGIN_RIGHT,
+          }}
+          {...rest}
+        >
+          ✓
+        </SymbolButton>
+      </Animated.View>
+    );
+  }
+}
 
 export default DoneButton;
